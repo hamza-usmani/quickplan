eeb5641a66e1d9088324afc197bddeea8405c16f
Hamza Usmani <hamzau97@hotmail.com>
2018-09-05 17:22:49 -0400

    updating packages

--- server.js
+++ server.js
@@ -4,15 +4,8 @@ const path = require('path');
 
 const app = express();
 
-console.log('serve static files');
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist/quick-plan'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-    console.log('send files');
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
-
-console.log('start the app');
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
