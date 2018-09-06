00a62644c2d8313554cd059e037d303cd3178257
Hamza Usmani <hamzau97@hotmail.com>
2018-09-06 00:16:24 -0400

    send index for all other routes

--- server.js
+++ server.js
@@ -7,5 +7,10 @@ const app = express();
 // Serve only the static files from the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+app.all('/*', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('index.html', { root: __dirname });
+});
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
